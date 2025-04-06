215ce4d42991ca80a8d93390b3561cf80963f2c5
Duda Nogueira <dudanogueira@gmail.com>
2025-04-05 21:36:35 -0300

    initial tests - triggering codecov

--- evoodoo/tests/test_controller.py
+++ evoodoo/tests/test_controller.py
@@ -2,7 +2,32 @@ from odoo.tests import tagged
 from odoo.tests.common import TransactionCase
 
 
-@tagged("standard", "at_install")
+@tagged("evoodoo")
 class TestSomething(TransactionCase):
     def test_basic(self):
         self.assertEqual(1 + 1, 2)
+
+
+@tagged("evoodoo", "controller")
+class TestControllerActiveInactive(TransactionCase):
+    def test_controller_active_inactive(self):
+        """
+        Test if controller returns 404 on inactive connectors
+        """
+        # create a active connector
+        self.env["evoodoo.connector"].create(
+            {
+                "name": "test_connector",
+                "type": "evolution",
+                "enabled": True,
+                "uuid": "76320171-94ec-455e-89c8-42995918fec6",
+                "url": "http://evolution:8080",
+                "api_key": "1234567890",
+            }
+        )
+        # send a request to that connector
+        # assert response is 200
+        # deactivate  the connector
+        # send a request to that connector
+        # assert response is 404
+        self.assertTrue(True)
