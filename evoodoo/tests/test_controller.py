50774d3042f17eb62b37af3e038adac6499a84bb
Duda Nogueira <dudanogueira@gmail.com>
2025-04-05 21:20:25 -0300

    filters and pre-commit

--- evoodoo/tests/test_controller.py
+++ evoodoo/tests/test_controller.py
@@ -1,7 +1,8 @@
-from odoo.tests.common import TransactionCase
 from odoo.tests import tagged
+from odoo.tests.common import TransactionCase
 
-@tagged('standard', 'at_install')
+
+@tagged("standard", "at_install")
 class TestSomething(TransactionCase):
     def test_basic(self):
         self.assertEqual(1 + 1, 2)
