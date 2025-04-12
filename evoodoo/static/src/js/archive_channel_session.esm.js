9998efb6410a5cc1af623062dfc43815fd0eafa6
Duda Nogueira <dudanogueira@gmail.com>
2025-04-12 10:45:47 -0300

    add confirmation to archive channel

--- evoodoo/static/src/js/archive_channel_session.esm.js
+++ evoodoo/static/src/js/archive_channel_session.esm.js
@@ -1,8 +1,8 @@
+import {ConfirmationDialog} from "@web/core/confirmation_dialog/confirmation_dialog";
 import {_t} from "@web/core/l10n/translation";
 import {threadActionsRegistry} from "@mail/core/common/thread_actions";
-// Import { useComponent } from "@odoo/owl";
-// import { useService } from "@web/core/utils/hooks";
-// import { ConfirmationDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
+import {useComponent} from "@odoo/owl";
+import {useService} from "@web/core/utils/hooks";
 
 threadActionsRegistry.add("archive-channel", {
     name: _t("Archive"),
@@ -17,39 +17,29 @@ threadActionsRegistry.add("archive-channel", {
         );
     },
     setup() {
-        // This.dialogService = useService("dialog");
-        // this.notification = useService("notification");
+        const component = useComponent();
+        component.dialogService = useService("dialog");
     },
     async open(component) {
         const thread = component.thread;
-        console.log("Channel ID:", thread?.id);
-        await component.store.env.services.orm.call(
-            "discuss.channel",
-            "action_unfollow",
-            [thread?.id]
-        );
-        await component.store.env.services.orm.call(
-            "discuss.channel",
-            "action_archive",
-            [thread?.id]
-        );
+        component.dialogService.add(ConfirmationDialog, {
+            body: _t(`Do you want to archive ${thread?.name} ?`),
+            title: _t("Archive Channel"),
+            confirm: async () => {
+                await component.store.env.services.orm.call(
+                    "discuss.channel",
+                    "action_unfollow",
+                    [thread?.id]
+                );
+                await component.store.env.services.orm.call(
+                    "discuss.channel",
+                    "action_archive",
+                    [thread?.id]
+                );
+            },
+            cancel: () => {
+                return true;
+            },
+        });
     },
 });
-
-// ThreadActionsRegistry.add("forward-channel", {
-//     name: _t("Forward"),
-//     icon: "fa fa-fw fa-hand-o-right text-success",
-//     iconLarge: "fa fa-fw fa-lg fa-hand-o-right text-success",
-//     sequence: 2,
-//     sequenceGroup: 100,
-//     condition: () => true,
-//     setup() {
-//         //const component = useComponent();
-//         this.notification = useService("notification");
-//         this.dialogService = useService("dialog");
-//     },
-//     async open(component) {
-//         // await component.store.env.services.orm.call("discuss.channel", "action_unfollow", [thread?.id]);
-//         // await component.store.env.services.orm.call("discuss.channel", "action_archive", [thread?.id]);
-//     },
-// });
