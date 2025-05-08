f79facf3d02b9a2b5d1ea5faff49e7514dcf1298
Duda Nogueira <dudanogueira@gmail.com>
2025-05-08 18:39:45 -0300

    [base] add test_get_or_create_partner_existing

--- discuss_hub/tests/test_base.py
+++ discuss_hub/tests/test_base.py
@@ -74,7 +74,6 @@ class TestBasePlugin(HttpCase):
         else:
             raise AssertionError("get_contact_name() should raise NotImplementedError")
 
-
     def test_get_or_create_partner_new(self):
         """
         Test creating a new partner when none exists
@@ -101,3 +100,39 @@ class TestBasePlugin(HttpCase):
         assert (
             result_partner.parent_id.name == "New Test Partner"
         ), "Parent partner should have the contact name"
+
+    def test_get_or_create_partner_existing(self):
+        """
+        Test retrieving an existing partner
+        """
+        # Mock the contact identifier method
+        self.plugin.get_contact_identifier = lambda payload: "1234567890"
+
+        # Set partner contact field
+        self.connector.partner_contact_field = "phone"
+        self.connector.partner_contact_name = "whatsapp"
+
+        # Create an existing partner
+        parent_partner = self.env["res.partner"].create(
+            {
+                "name": "Test Parent Partner",
+                "phone": "1234567890",
+            }
+        )
+
+        partner_contact = self.env["res.partner"].create(
+            {
+                "name": "whatsapp",
+                "phone": "1234567890",
+                "parent_id": parent_partner.id,
+            }
+        )
+
+        # Test get_or_create_partner
+        result_partner = self.plugin.get_or_create_partner(
+            payload={"test": "data"}, update_profile_picture=False
+        )
+
+        assert (
+            result_partner.id == partner_contact.id
+        ), "Should return the existing partner contact"
